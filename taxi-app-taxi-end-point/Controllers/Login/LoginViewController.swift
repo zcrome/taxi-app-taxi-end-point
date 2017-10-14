9ae9aa9ef47e9f90451bd397a666fd1e63f97a8a
zcrome <soportezcrome@gmail.com>
2017-10-14 17:23:58 -0500

    Primer commit

---
+++ taxi-app-taxi-end-point/Controllers/Login/LoginViewController.swift
@@ -0,0 +1,60 @@
+//
+//  LoginViewController.swift
+//  taxi-app-client-end-point
+//
+//  Created by zcrome on 10/14/17.
+//  Copyright © 2017 zcrome. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+  
+  //**************************************
+  //**** MARK: - Constraints outlets
+  //**************************************
+  
+  //**************************************
+  //**** MARK: - View outlets
+  //**************************************
+  
+  //**************************************
+  //**** MARK: - Local constans and variables
+  //**************************************
+  
+  //**************************************
+  //**** MARK: - ViewController override definitions
+  //**************************************
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  override func viewDidLayoutSubviews() {
+    super.viewDidLayoutSubviews()
+  }
+  
+  //**************************************
+  //**** MARK: - Actions and other outlets remaining
+  //**************************************
+  
+  //**************************************
+  //**** MARK: - Additional functions that may be require
+  //**************************************
+
+}
+
+
+
+
+
+
+
